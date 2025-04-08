c0da76be268e0c81a8c534c36bdf2b9e2dbf0b56
JSongProvision <childrenofthefenceunite@gmail.com>
2025-04-08 13:16:14 -0400

    Complete Ex 3
    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let reversedString = '';
+    while (stringToReverse.length > 0) {
+        reversedString += stringToReverse[stringToReverse.length -1];
+        stringToReverse = stringToReverse.slice(0, stringToReverse.length -1);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
