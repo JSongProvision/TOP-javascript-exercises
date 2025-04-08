14face59279b560d299148010bdbdef17ebddf5f
JSongProvision <childrenofthefenceunite@gmail.com>
2025-04-07 22:55:55 -0400

    Complete Ex 2
    Completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let concatString = "";
+    while (num > 0) {
+        concatString += string;
+        num--;
+    }
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        return concatString;
+    }
 };
 
 // Do not edit below this line
