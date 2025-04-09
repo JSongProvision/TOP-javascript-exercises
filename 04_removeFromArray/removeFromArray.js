cff259ed3409d4f14816e2575ecf511e5aa58c9a
JSongProvision <142438994+JSongProvision@users.noreply.github.com>
2025-04-09 14:25:36 -0400

    Finish Exercise
    completed all tests, removed console logs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,9 +2,7 @@ const removeFromArray = function(givenArray, ...args) {
     while (args.length > 0) {
         if (givenArray.indexOf(args[0]) != -1) {
             givenArray.splice(givenArray.indexOf(args[0]),1);
-            console.log("I hit the if");
         } else {
-            console.log("I hit the else");
             args.shift();
         }   
     }
