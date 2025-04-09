d3f5f3e93d36b66bb56e12088d89a24ce1baf923
JSongProvision <142438994+JSongProvision@users.noreply.github.com>
2025-04-09 14:20:37 -0400

    Pass First Test
    Have baseline code that passes the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray, ...args) {
+    while (args.length > 0) {
+        if (givenArray.indexOf(args[0]) != -1) {
+            givenArray.splice(givenArray.indexOf(args[0]),1);
+            console.log("I hit the if");
+        } else {
+            console.log("I hit the else");
+            args.shift();
+        }   
+    }
+    return givenArray;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
