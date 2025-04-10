2f109e1b8268613de4803cb5c4943ad7b072ae90
JSongProvision <childrenofthefenceunite@gmail.com>
2025-04-09 22:05:00 -0400

    Add base case requirements
    Added functionality to past tests 1 - 4. Fails on test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
+    let counter = 0;
 
+    if(startNum < 0 || endNum < 0) {
+        return 'ERROR';
+    } else if(startNum < endNum) {
+        counter = startNum;
+        while(counter <= endNum) {
+            sum += counter;
+            counter++;
+        }
+        return sum;
+    } else if(startNum > endNum){
+        counter = endNum;
+        while(counter <= startNum) {
+            sum += counter;
+            counter++;
+        }
+        return sum;
+    }   else {
+        return 'There has been a catastrophic failure in my pride';
+    }
+    
 };
 
 // Do not edit below this line
