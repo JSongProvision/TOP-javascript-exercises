47f551dac7bde7ff583aa5b2d96587a30697af7c
JSongProvision <childrenofthefenceunite@gmail.com>
2025-04-12 21:43:18 -0400

    Complete Ex 7
    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(startTemp) {
+  let convertedTemp = (startTemp - 32) * (5/9);
+  return Math.round(convertedTemp*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(startTemp) {
+  let convertedTemp = startTemp * (9/5) + 32;
+  return Math.round(convertedTemp*10)/10;
 };
 
 // Do not edit below this line
